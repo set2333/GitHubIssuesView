27f9a8355f4c6387500098b24d445447dfddd630
set2333 <set2333@mail.ru>
2020-02-26 09:03:52 +0700

    Мини инструкция в readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Выполнение тестового задания https://docs.google.com/document/d/1aRu2marUZqj2Nyu00GK5Y1-OYWM4I5OexE28SUyZ3LM
+
+Запуск в режиме разработки: npm run dev
+
+Запус в собранном режиме: npm run start
--- index_build.js
+++ index_build.js
@@ -7,4 +7,7 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index_build.html');
 });
 
-app.listen(8080, ()=>console.log('Server started...'));
+app.listen(8080, ()=>{
+    console.log('Что-то менял? Не забудь выполнить NPM RUN BUILD!!!');
+    console.log('Server started...');
+});
