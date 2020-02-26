31414c587e5f634bdf59a18bc7d389f5d69cd03d
set2333 <set2333@mail.ru>
2020-02-26 08:29:12 +0700

    Разделил dev и build сборки.

---
+++ index_build.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index_build.html');
+});
+
+app.listen(8080, ()=>console.log('Server started...'));
